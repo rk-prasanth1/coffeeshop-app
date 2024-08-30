1f431fefc8b9795dafe5129e5feed27152a4501f
rk-prasanth1 <prasanth.rk44@gmail.com>
2024-08-30 15:34:57 +0530

    k8s

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,15 +4,17 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensure this is set to 'dist' or your desired output directory
+  },
   server: {
     proxy: {
       '/api': {
         target: 'http://localhost:4000',
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 });
-
